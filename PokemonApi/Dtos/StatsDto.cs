d3b31ddfabadb7bfdfaaeefdffe1bb18267afb67
Leonardo-Negrete <leodanielnc@gmail.com>
2025-02-18 21:01:19 -0600

    Repositorio actualizado

---
+++ PokemonApi/Dtos/StatsDto.cs
@@ -0,0 +1,13 @@
+using System.Runtime.Serialization; //Esta línea importa el namespace System.Runtime.Serialization, el cual proporciona atributos y utilidades para la serialización y deserialización de objetos en C#
+
+namespace PokemonApi.Dtos;
+
+[DataContract(Name = "StatsDto", Namespace = "http://pokemon-api/pokemon-service")] //esto es lo que le dara un orden (serializara) al documento xml
+public class StatsDto{
+    [DataMember(Name = "Attack", Order = 1)] //Hara que en el xml aparezca asi: "Attack": 55 y el order=1 Especifica el orden en que aparecerá en la salida serializada.
+    public int Attack {get; set;}
+    [DataMember(Name = "Defense", Order = 2)]
+    public int Defense {get; set;}
+    [DataMember(Name = "Speed", Order = 3)]
+    public int Speed {get; set;}
+}
---
+++ PokemonApi/Mappers/PokemonMappers.cs
@@ -0,0 +1,38 @@
+using PokemonApi.Dtos;
+using PokemonApi.Infrastructure.Entities;
+using PokemonApi.Models;
+
+namespace PokemonApi.Mappers;
+
+public static class PokemonMapper{
+    public static Pokemon ToModel(this PokemonEntity entity){
+        if(entity is null){
+            return null;
+        }
+        return new Pokemon{
+            id = entity.Id,
+            Name = entity.Name,
+            Level = entity.Level,
+            Type = entity.Type,
+            Stats = new Stats{
+                Attack = entity.Attack,
+                Defense = entity.Defense,
+                Speed = entity.Speed
+            }
+        };
+    }
+
+    public static PokemonResponseDto ToDto(this Pokemon pokemon){
+        return new PokemonResponseDto{
+            Id = pokemon.id,
+            Level = pokemon.Level,
+            Name = pokemon.Name,
+            Type = pokemon.Type,
+            Stats = new StatsDto {
+                Attack = pokemon.Stats.Attack,
+                Speed = pokemon.Stats.Speed,
+                Defense = pokemon.Stats.Defense
+            }
+        };
+    }
+}
